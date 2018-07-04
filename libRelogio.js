a31613b563ebf084f9f0de4a058cca692f97217a
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-07-04 10:09:32 -0300

    Exercicios da parte da manha

--- libRelogio.js
+++ libRelogio.js
@@ -1,6 +1,6 @@
-var tempo
-function formataRelogio(tempo){
-    var tempoEmHorasFloat = tempo/60
+var tempoEmMinutos
+function formataRelogio(){
+    var tempoEmHorasFloat = tempoEmMinutos/60
     var horas = parseInt(tempoEmHorasFloat)
     var minutos = Math.ceil((tempoEmHorasFloat - horas) * 60)
     return horas.toString().padStart(2,0) + ":" + minutos.toString().padStart(2,0)
---
+++ numero.js
@@ -0,0 +1,6 @@
+//Programa feito por Lucas Takeshi
+//Email lucas.takeshi@caelum.com.br
+var frase
+function pegaNumero() {
+    return parseFloat( prompt(frase).replace(",", ".") )
+}
