7ed97dc953cf6d210aea89c8f18386da3313e5a7
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-07-05 18:12:23 -0300

    Ultimos exercicios

--- mostra.js
+++ mostra.js
@@ -1,6 +1,5 @@
-function mostra(mensagem, classe = ""){
+function mostra(mensagem){
     var tag = document.createElement("p")
     tag.innerHTML = mensagem
-    tag.classList.add(classe)
     document.body.appendChild(tag)
 }
